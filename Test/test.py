d1b38d68208ea6143d5ef44938d64163355abb58
Brendan <brendanmarko@gmail.com>
2017-10-13 14:50:52 -0400

    Init 0.04

---
+++ Test/test.py
@@ -0,0 +1,47 @@
+# This is a simple program that takes input from the command line and processes it accordingly
+# October 23, 2017 
+
+# Import allow us to use functions from pre-written packages
+# The format is: import package-name
+import sys
+
+# We can pass information to a program when it runs, this allows us to pass imformation from other scripts
+# We check the number of command line arguments with the variable:
+num_args=len(sys.argv)
+
+# This declares an empty list that we can add to later
+# A list is different from an array in the sense it can grow; array size is fixed
+arg_storage=[]
+
+# Now that we have declared some variables lets make a function to check what the script was passed
+def argHandler():
+    
+    # Checks if no arguments were passed
+    # We check num_args == 1 because when we execute a script the first parameter is the file name
+    # ie) sys.argv[0] == test.py
+    if (num_args == 1):
+        print("No arguments passed")
+    
+    # This section executes if the first statement fails
+    else:
+        # Here we iterate over the arguments passed and add them to our list: arg_storage
+        # We already stored the number of argumemts so we have our bound for looping
+        for x in range(num_args):
+            arg_storage.append(sys.argv[x])
+
+# Here we are calling the function we declared earlier
+# Notice the empty brackets; if the function had parameters we must match the function prototype declared
+argHandler()
+
+# Once we have built our list we print the contents
+# For that we will make another function to print it
+# Notice the function takes a parameter this time; in this case a list
+def argPrinter(temp_list = []):
+
+    # Loops over the list we defined earlier: arg_storage
+    for x in range(len(temp_list)):
+        print(temp_list[x])
+
+# Now we want to call our print function and print the list contents
+# When we call our function we need to pass our list as the parameter
+argPrinter(arg_storage)
