ed57a453c43840ffb3be4bac5d12fb5afd2b0c0e
brendanmarko <brendanmarko@gmail.com>
2017-10-13 15:17:05 -0400

    Init 0.05

--- Test/test.py
+++ Test/test.py
@@ -1,31 +1,30 @@
-# This is a simple program that takes input from the command line and processes it accordingly
-# October 23, 2017 
+# This is a simple program that takes input from the command line and prints the arguments
 
-# Import allow us to use functions from pre-written packages
+# Import allow us to use functions and variables from pre-written packages
 # The format is: import package-name
 import sys
 
-# We can pass information to a program when it runs, this allows us to pass imformation from other scripts
+# We can pass information to a program when it runs; this allows us to pass information to the script at runtime
 # We check the number of command line arguments with the variable:
 num_args=len(sys.argv)
 
-# This declares an empty list that we can add to later
+# Here we declares an empty list that we can add to later
 # A list is different from an array in the sense it can grow; array size is fixed
 arg_storage=[]
 
-# Now that we have declared some variables lets make a function to check what the script was passed
+# Now that we have declared some variables we make a function to check what args the script received
 def argHandler():
     
     # Checks if no arguments were passed
     # We check num_args == 1 because when we execute a script the first parameter is the file name
-    # ie) sys.argv[0] == test.py
+    # ie) sys.argv[0] == test.py; from the command line 'python test.py'
     if (num_args == 1):
         print("No arguments passed")
     
     # This section executes if the first statement fails
     else:
         # Here we iterate over the arguments passed and add them to our list: arg_storage
-        # We already stored the number of argumemts so we have our bound for looping
+        # We already stored the number of arguments, so we have our bound for looping
         for x in range(num_args):
             arg_storage.append(sys.argv[x])
 
@@ -33,8 +32,7 @@ def argHandler():
 # Notice the empty brackets; if the function had parameters we must match the function prototype declared
 argHandler()
 
-# Once we have built our list we print the contents
-# For that we will make another function to print it
+# Once we have built our list we print the contents, to do this we will make another function to print it
 # Notice the function takes a parameter this time; in this case a list
 def argPrinter(temp_list = []):
 
