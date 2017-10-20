b70154312c4dbc2f1f774d679fb147925840df6c
Brendan <brendanmarko@gmail.com>
2017-10-20 16:23:25 -0400

    Race 0.05

--- Race/Code/Main.py
+++ Race/Code/Main.py
@@ -10,8 +10,10 @@ level_num=""
 # Reads arguments from command line for which level to load
 # If no argument is entered, 00 is rendered
 if (len(sys.argv) > 1):
-	level_num=sys.argv[1]
+    level_num=sys.argv[1]
 else:
-	level_num="00"
+    level_num="00"
 
 curr_level = LevelReader(level_num)
+curr_level.setup()
+curr_level.printLayout()
